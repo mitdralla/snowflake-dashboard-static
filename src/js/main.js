bca6d43a51c29be97d58d619d54d2e0ae67e5795
Timothy Allard <timallard@gmail.com>
2019-02-11 16:44:38 -0600

    carousel edits, font sizes

--- src/js/main.js
+++ src/js/main.js
@@ -2,27 +2,29 @@ $(document).ready(function(){
 
     // Main dApp Slider
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        responsiveClass:true,
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
         responsive:{
             0:{
-                items:1,
-                nav:true
+                items: 1,
+                nav: true,
+                loop: true
             },
             600:{
-                items:3,
-                nav:false
+                items: 3,
+                nav: false,
+                loop: true
             },
             1000:{
-                items:5,
-                nav:true,
-                loop:false
+                items: 5,
+                nav: true,
+                loop: true
             }
         },
-        autoplay:true,
-        autoplayTimeout:3000,
-        autoplayHoverPause:false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: false,
         autoWidth: true,
         navigation: false
     })
